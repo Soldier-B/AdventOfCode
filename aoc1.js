a2f50812a81ae5302af47d0a58be3e5bcb94c1ce
Brian Maxwell <Soldier-B@users.noreply.github.com>
2016-12-02 12:25:46 -0500

    Create aoc1.js

---
+++ aoc1.js
@@ -0,0 +1,13 @@
+function AoC_1(directions){
+	var a = 0, x = 0, y = 0;
+	directions.split(', ').map(function(direction){
+		var mult = direction[0] == 'L' ? -1 : 1,
+			steps = +direction.slice(1);
+		a += Math.PI * mult / 2;
+		x += Math.round(Math.cos(a) * steps);
+		y += Math.round(Math.sin(a) * steps);
+	});
+	return Math.abs(x) + Math.abs(y);
+}
+
+// AoC_1('L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1');
