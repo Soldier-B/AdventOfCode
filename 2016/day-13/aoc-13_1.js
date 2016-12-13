f7bc43ed4f7a0ca33aa4929595a98600625951f5
Soldier-B <bdmaxwell@gmail.com>
2016-12-13 13:23:33 -0500

    2016, day 13

---
+++ 2016/day-13/aoc-13_1.js
@@ -0,0 +1,30 @@
+function AoC_13_1(){
+	for(var nodes = [node(1, 1)], steps = 0, visited = new Set();; steps++){
+		if(nodes.length && nodes.filter(function(n){ return n.x == 31 && n.y == 39; }).length)
+			return steps;
+		nodes.forEach(spawn);
+		nodes = nodes.filter(prune);
+	}
+	
+	function node(x, y){ 
+		return { x: x, y: y, key: x + ',' + y }; 
+	}
+	
+	function spawn(n){
+		visited.add(n.key);
+		nodes.push(node(n.x, n.y - 1));
+		nodes.push(node(n.x, n.y + 1));
+		nodes.push(node(n.x - 1, n.y));
+		nodes.push(node(n.x + 1, n.y));
+	}
+	
+	function prune(n){
+		if(visited.has(n.key) || n.x < 0 || n.y < 0) return false;
+		return ((n.x * n.x) + (3 * n.x) + (2 * n.x * n.y) + n.y + (n.y * n.y) + 1352)
+			.toString(2)
+			.match(/1/g)
+			.length % 2 == 0;
+	}
+}
+
+AoC_13_1();
