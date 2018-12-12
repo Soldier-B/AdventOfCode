58819e61769128f121e49b2e14373ab0d4322094
Brian Maxwell <Soldier-B@users.noreply.github.com>
2018-12-11 23:23:14 -0500

    updated day 9 to use a linked list for performance

--- 2018/day-09/aoc_09.js
+++ 2018/day-09/aoc_09.js
@@ -1,28 +1,102 @@
-function aoc_9(p, m){
-	var players = Array(p).fill(0),
-		marbles = [0],
+function aoc_9(p, m) {
+	var players = new LinkedList(),
+		marbles = new LinkedList(),
+		currentPlayer,
+		currentMarble,
 		next = 1;
-		
-	players.current = 0;
-	marbles.current = 0;
-		
-	while(next <= m){
-		var i = ((marbles.current + 1) % marbles.length) + 1;
-		
-		if(next % 23)
-			marbles.splice(i, 0, next);
-		else{
-			i = marbles.current - 7;
-			if(i < 0) i += marbles.length;
-			players[players.current] += next + marbles.splice(i, 1)[0];
+
+	for (var i = 0; i < p; i++)
+		players.addLast(0);
+
+	currentPlayer = players.head;
+	currentMarble = marbles.addLast(0);
+
+	while (next <= m) {
+		if (next % 23)
+			currentMarble = marbles.addAfter(next++, currentMarble.next);
+		else {
+			for (var i = 0; i < 7; i++)
+				currentMarble = currentMarble.previous;
+			currentPlayer.value += next++ + currentMarble.value;
+			currentMarble = currentMarble.next;
+			marbles.remove(currentMarble.previous);
 		}
-		
-		marbles.current = i;
-		next++;
-		players.current = ++players.current % players.length;
+
+		currentPlayer = currentPlayer.next;
 	}
-	
-	console.log(Math.max.apply(0, players));
+
+	console.log(Math.max.apply(0, players.toArray()));
+}
+
+// partial circular doubly linked list implementation
+function LinkedList() {
+	var me = this;
+
+	this.head = null;
+
+	function initList(node) {
+		node.next = node.previous = node;
+		me.head = node;
+
+	}
+
+	function insertBefore(node, before) {
+		node.next = before;
+		node.previous = before.previous;
+		before.previous.next = node;
+		before.previous = node;
+	}
+
+	this.addLast = function (value) {
+		var node = Node(value);
+
+		if (this.head == null)
+			initList(node);
+		else
+			insertBefore(node, me.head)
+
+		return node;
+	};
+
+	this.addAfter = function (value, after) {
+		var node = Node(value);
+
+		insertBefore(node, after.next);
+
+		return node;
+	};
+
+	this.remove = function (node) {
+		if (node.next == node)
+			head = null;
+		else {
+			node.next.previous = node.previous;
+			node.previous.next = node.next;
+			if (me.head == node)
+				me.head = node.next;
+		}
+
+	}
+
+	this.toArray = function () {
+		var arr = [],
+			node = me.head;
+
+		do {
+			arr.push(node.value);
+			node = node.next;
+		} while (node.next != me.head)
+
+		return arr;
+	}
+
+	function Node(value) {
+		return {
+			previous: null,
+			next: null,
+			value: value
+		};
+	}
 }
 
 // 486 players; last marble is worth 70833 points
